07b69b0ceb25a0b8f35b198825874929540392ca
GermanRMurua <german-r-m@outlook.com>
2022-04-23 19:26:01 -0300

    +progresos en la pagina - agregar info y enlaces en menu y footer

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-const german = ['alto', 'castaño', 'flaco'];
+const iconoMenu = document.querySelector('#icono-menu'),
+        menu = document.querySelector('#menu');
+
+iconoMenu.addEventListener('click', (e)=> {
+
+    menu.classList.toggle('active');
+
+    const rutaActual = e.target.getAttribute('src');
+
+    if(rutaActual == './img/images.jpg'){
+        e.target.setAttribute('src', './img/images.jpg');
+    } else{
+        e.target.setAttribute('src', './img/images.jpg');
+
+    }
+});
